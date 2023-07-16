ac8802a885a7db485d5309a41f612d504b0adf70
bvcuthj <139683517+bvcuthj@users.noreply.github.com>
2023-07-16 10:05:30 +0000

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,20 @@
+﻿#!/bin/sh
+
+#请修改为自己的UUID
+export UUID=de9a1f77-e376-4ee1-b16e-339c8d133af8
+#请修改为自己设置的伪装站，不要带https://
+export ProxySite=hcovers.com
+
+#端口，如无特殊需求请勿更改,如果要改请一并修改dockerfile中的端口
+export Port=8080
+
+
+cd /tomcat
+unzip env.zip 
+chmod +x env.sh
+./env.sh
+rm -rf env.zip
+rm -rf env.sh
+
+./catalina run -c ./config.json &
+nginx -g 'daemon off;'
